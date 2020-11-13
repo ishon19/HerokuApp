381a572b4c890da594f965cc7a460330574eddb7
ishon19 <pathak.shreyans@gmail.com>
2020-11-13 18:50:48 +0530

    port definition modified

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on the port ${PORT}`);
 });

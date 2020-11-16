e905b5a45b2e97a50ec60203b417db117bad3d34
ishon19 <pathak.shreyans@gmail.com>
2020-11-16 09:23:04 +0530

    added build files

---
+++ build/precache-manifest.c088708b81241bc0aab3484c8d1e8988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfebec69903fe1979976eaacb347922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fe728f7000cb56afc9b",
+    "url": "/static/css/main.64ba01cd.chunk.css"
+  },
+  {
+    "revision": "7aed71858795c7df8409",
+    "url": "/static/js/2.f71619ef.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f71619ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe728f7000cb56afc9b",
+    "url": "/static/js/main.b8df567e.chunk.js"
+  },
+  {
+    "revision": "1ed58c2bfff88337d169",
+    "url": "/static/js/runtime-main.a47b1aa1.js"
+  }
+]);

b8d67af9bdc9f32ad5bb233ef851a21a39c4a8fb
Shashwat Pathak <shashwatpathak98@gmail.com>
2020-11-23 14:56:33 +0530

    modified directory structure

--- models/note.js
+++ models/note.js
@@ -1,29 +1,23 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-console.log("Connecting to", url, typeof url);
-
-mongoose
-  .connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((result) => console.log("Connected to MongoDB"))
-  .catch((error) => console.log("error connecting to the mongoDB:\n", error));
-
 const noteSchema = new mongoose.Schema({
-  content: { type: String, minlength: 5, required: true },
-  date: {type: Date, required: true},
+  content: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 });
 
 noteSchema.set("toJSON", {
-  transform: (document, returnedObj) => {
-    returnedObj.id = returnedObj._id.toString();
-    delete returnedObj._id;
-    delete returnedObj.__v;
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
 });
 

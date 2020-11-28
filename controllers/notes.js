3daf11a702a97a054ba21a50d84a7ba90e298648
ishon19 <pathak.shreyans@gmail.com>
2020-11-28 09:07:33 +0530

    more tests added

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
-notesRouter.get("/", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
 notesRouter.get("/:id", (request, response, next) => {
@@ -19,7 +18,7 @@ notesRouter.get("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-notesRouter.post("/", (request, response, next) => {
+notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
   const note = new Note({
@@ -28,12 +27,12 @@ notesRouter.post("/", (request, response, next) => {
     date: new Date(),
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  try {
+    const savedNote = await note.save();
+    response.json(savedNote.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
 notesRouter.delete("/:id", (request, response, next) => {

2ec82614661b7a7dbee279006521cf7e46d3951e
ishon19 <pathak.shreyans@gmail.com>
2020-11-29 10:02:43 +0530

    JWT auth done

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,19 @@
+/* eslint-disable no-undef */
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -17,14 +28,24 @@ notesRouter.get("/:id", async (request, response) => {
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   response.json(savedNote.toJSON());
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -22,7 +22,7 @@ usersRouter.post("/", async (request, response, next) => {
 });
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("notes", { content: 1, date: 1 });
   response.json(users);
 });
 
